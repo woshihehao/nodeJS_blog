963e72bb2c6fe74ddff0e69eb8ef650a499bf24b
hehao <2791566231@qq.com>
2015-08-02 13:03:35 +0800

    add_blog

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
-var http = require("http");
+var fs = require('fs');
+
+var body_parser = require('body-parser');
+
+var iconv = require('iconv-lite');// 加载编码转换模块
 //以上是所需要的模块
+var express = require('express');
 var app = express();
 //创建express的实例
 
+app.use(body_parser());
 
-var express = require('express');
-var server = http.createServer();
-var app = express();
+
+
 
 app.get('/', function(req, res) {
-
+    console.log("Hello world")
 });
 
 app.post('/register', function(req, res) {
@@ -31,6 +36,23 @@ app.get('/about', function(req, res) {
 
 app.post('/add_blog', function(req, res) {
 
+    var title = req.body.title;
+    var body = req.body.body;
+    var json_string = fs.readFileSync('blog.json').toString();
+    var blogs = eval('(' + json_string + ')');
+
+
+    blogs.next_id += 1;
+    var blog = {
+        id: blogs.next_id,
+        title: title,
+        body: body
+    };
+    blogs.articles.push(blog);
+
+    fs.writeFile("blog.json", JSON.stringify(blogs), 'utf8', function() {
+        console.log("Stored in file");
+    });
 });
 
 app.post('/edit_blog/:number',function(req, res) {
@@ -53,6 +75,4 @@ app.get('/query/:condition', function(req, res) {
 
 });
 
-
-
-server.listen(8888);
+app.listen(3000);
