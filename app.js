cc6b2a4748497756b08f0a9b6a439172136b5a17
hehao <2791566231@qq.com>
2015-08-03 20:21:42 +0800

    well

--- app.js
+++ app.js
@@ -1,71 +1,108 @@
 var express = require('express');
 var fs = require('fs');
-var models = require('./models');
-
+var model = require('./models');
+var mysql = require('mysql');
 var body_parser = require('body-parser');
 
+
+var session = require('express-session');
+
 //以上是所需要的模块
-var express = require('express');
+
 var app = express();
 //创建express的实例
 
-app.use(body_parser());
+app.use(body_parser());//读取POST内容
+
+
+app.use(session({
+    secret: '0GBlJZ9EKBt2Zbi2flRPvztczCewBxXK'
+}));
+var user = new model.User();
+var blog = new model.Blog();
 
 
 app.get('/', function (req, res) {
     console.log("Hello world")
 });
-
+var check_out_user = function (req, res, next) {
+    if (req.session.username) {
+        next();
+    } else {
+        res.send("you have no right to take the operation\n");
+    }
+};
 app.post('/register', function (req, res) {
-
+    username = req.body.username;
+    password = req.body.password;
+    user.register(username,password);
+    res.send("OK");
 });
 
 app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    user.login(username, password, function (id) {
+        if (id != -1) {
+            req.session.user_id = id;
+            res.send("you are logged in successfully\n");
+        } else {
+            res.send("username doesn't match password\n");
+        }
+    });
+
 
 });
 
-app.post('/logout', function (req, res) {
-
+app.post('/logout',check_out_user, function (req, res) {
+     req.session.user_id = NULL;
+    res.redirect('/login');
 });
 
-app.get('/about', function (req, res) {
 
-});
 
-app.post('/add_blog', function (req, res) {
+app.post('/add_blog',check_out_user, function (req, res) {
 
     var title = req.body.title;
-    var body = req.body.body;
-    var blogs = models.load_blogs();
-    models.add_blog(blogs,title,body);
-    models.store_blogs(blogs)
+    var content = req.body.content;
+
+
+    blog.new_post(title, content);
+
 
     res.send("OK");
 });
 
-app.post('/edit_blog/:id', function (req, res) {
+
+
+app.post('/del_blog/:id', check_out_user,function (req, res) {
+    var id = req.param("id");
+
+
+    blog.delete(Number(req.param('id')));
+
+    res.send("delete");
+
 
 });
 
-app.post('/del_blog/:id', function (req, res) {
 
-});
-
-app.get('/archives/:id', function (req, res) {
-
-});
 
 app.get('/tags/:tag_name/:id', function (req, res) {
 
 });
 
-app.get('/query/:id', function (req, res) {
+app.get('/query/:id',check_out_user, function (req, res) {
 
-    var id = req.param("id");
+    blog.query('id',req.param('id'));
 
-    var blog = models.query_title( id);
-
-    res.send(JSON.stringify(blog));
 });
-
+app.post('/edit_blog/:id', check_out_user,function (req, res) {
+    blog.query('id', req.param('id'));
+    var title = req.body.title;
+    var content = req.body.content;
+    blog.edit(Number(req.param('id')), title, content);
+    blog.query('id', req.param('id'));
+});
 app.listen(3000);
--- models.js
+++ models.js
@@ -2,38 +2,105 @@
  * Created by hehao on 15/8/2.
  */
 var fs = require('fs');
-module.exports = {
-    query_title: function (blogs,id) {
 
-        if (id>blogs.next_id){
-            return null
-        }
-        for (var i = 0; i < blogs.articles.length; i++) {
-            if (blogs.articles[i].id == id) {
-                return blogs.articles[i];
-            }
-        }
-        return null;
-    },
+exports.User = function () {
+    this.mysql = require('mysql');
+    this.connection = this.mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nodejs'
+    });
+    this.connection.connect();
+};
 
-    add_blog: function (blogs, title, body) {
 
-        blogs.next_id += 1;
-        var blog = {
-            id: blogs.next_id,
-            title: title,
-            body: body
-        };
-        blogs.articles.push(blog);
-    },
+exports.User.prototype.login = function (username, password,set_id) {
+    var str =
+        'SELECT * FROM user WHERE username="'
+        + username + '"and password="' + password + '";';
+    console.log(str);
+    var id = -1;
+    this.connection.query(str, function(error, results, fields) {
+        if (results.length)
+            set_id(results[0].user_id);
+        else
+            set_id(-1);
+    });
+};
 
-    load_blogs: function () {
-        var json_string = fs.readFileSync('blog.json').toString();//单线程读出，
-        var blogs = eval('(' + json_string + ')');//将读出的字符串转化为对象
-        return blogs;
-    },
+exports.User.prototype.register = function (username, password) {
+    var str =
+        'INSERT INTO user VALUES(NULL, "' + username + '", "' + password + '");';
+    console.log(str);
+    this.connection.query(str);
+};
 
-    store_blogs:function(blogs){
-        fs.writeFileSync("blog.json", JSON.stringify(blogs));//单线程写入，将blogs对象转化为字符串
-    }
+
+exports.Blog = function () {
+    this.sql = require('mysql');
+    this.connection = this.sql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nodejs'
+    });
+    this.connection.connect();
+};
+
+
+exports.Blog.prototype.register = function (username, password) {
+    var str =
+        'insert into user values (NULl,"' + username + '","' + password + '")';
+    console.log(str);
+    this.connection.query(str, function (error, results) {
+        console.log(results)
+    })
+};
+
+//exports.Blog.prototype.login = function (username, password) {
+        //var str =
+            //'SELECT * FROM user WHERE username="' + username + '" and password= "' + password + '"';
+    //this.connection.query(str, function (error, results) {
+        //if (error)
+            //console.log(error);
+        //else if (results == "" || results == null) {
+            //req.session.error = '用户名或密码不正确';
+        //}
+        //else {
+            //req.session.error = 'username';
+            //console.log(results);
+        //}
+    //});
+//};
+exports.Blog.prototype.query = function (id) {
+    var str =
+        'SELECT * FROM blog WHERE id ="' + id + '";';
+    console.log(str);
+    this.connection.query(str, function (error, results) {
+        console.log(results);
+    });
+};
+
+exports.Blog.prototype.new_post = function (title, content) {
+
+    var str =
+        'INSERT INTO blog VALUES(NULL, "' + title + '", "' + content + '");';
+    console.log(str);
+    this.connection.query(str);
+};
+
+
+exports.Blog.prototype.edit = function (id, title, content) {
+    var str =
+        'UPDATE blog SET title="' + title + '", content="' + content + '"' + 'WHERE id=' + id + ';';
+    console.log(str);
+    this.connection.query(str);
+};
+
+exports.Blog.prototype.delete = function (id) {
+    var str =
+        'DELETE FROM blog WHERE id=' + id + ';';
+    console.log(str);
+    this.connection.query(str);
 };
